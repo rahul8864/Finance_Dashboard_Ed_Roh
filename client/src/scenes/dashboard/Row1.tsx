61a934644a53d40a657bcedb8fad4112bb3bd4ef
Rahul Kumar <rahul@aerosimple.in>
2023-05-22 22:24:01 +0530

    Row1 Complete

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,15 +1,21 @@
 import DashboardBox from '@/components/DashboardBox';
 import { useGetKpisQuery } from '@/state/api';
-import { Area, AreaChart, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { useMemo } from 'react';
 import { useTheme } from '@mui/material/styles';
 import BoxHeader from '@/components/BoxHeader';
 
-type Props = {}
-
-const Row1 = (props: Props) => {
+const Row1 = () => {
     const { palette } = useTheme();
     const { data } = useGetKpisQuery();
+    const revenue = useMemo(() => {
+        return data && data[0].monthlyData.map(({ month, revenue }) => {
+            return {
+                name: month.substring(0, 3).toUpperCase(),
+                revenue: revenue
+            }
+        })
+    },[data])
     const revenueExpenses = useMemo(() => {
         return data && data[0].monthlyData.map(({ month, revenue, expenses }) => {
             return {
@@ -87,14 +93,29 @@ const Row1 = (props: Props) => {
                 </ResponsiveContainer>
             </DashboardBox>
             <DashboardBox bgcolor="#fff" gridArea="c">
+                <BoxHeader
+                    title="Revenue Month by Month"
+                    subtitle="graph representing the revenue month by month"
+                    sideText='+4%'
+                />
                 <ResponsiveContainer width="100%" height="100%">
-                    <BarChart width={500} height={300} data={data} margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
+                    <BarChart width={500} height={300} data={revenue} margin={{
+                        top: 17,
+                        right: 15,
+                        left: -5,
+                        bottom: 58,
                     }}>
-
+                        <defs>
+                            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.8} />
+                                <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
+                        <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+                        <XAxis dataKey="name" axisLine={false} tickLine={false} style={{ fontSize: "10px" }} />
+                        <YAxis axisLine={false} tickLine={false} style={{ fontSize: "10px" }} />
+                        <Tooltip />
+                        <Bar dataKey="revenue" fill="url(#colorRevenue)" />
                     </BarChart>
                 </ResponsiveContainer>
             </DashboardBox>
